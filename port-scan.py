ff3fe6950d1c343ae4977292be148f5dd4026f1a
sigwo <me@sigwo.com>
2013-08-31 12:48:39 -0500

    Added disclaimer in case it blows up.
    Basically, I'm not responsible.

--- port-scan.py
+++ port-scan.py
@@ -1,4 +1,15 @@
-#Script by Sigwo
+# 
+# Script by Steven Grove (@sigwo)
+#           www.sigwo.com
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
 # 21Jan13
 #
 #Raw TCP port scanner. You will need the tcp-port-scan.py file in the same 
@@ -13,7 +24,7 @@ for series in Series:
     os.system( ffile )
 
 #Now time to do something with the results in a future release   
-#file = open("Results.csv")
+#file = open("results.csv")
 
 #for line in file:
 #    pass # do something
